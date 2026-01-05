4e6070ec5e89402873116c4e2518b72aa41b52e6
Goonerd17 <tlghks1217@naver.com>
2026-01-05 19:35:21 +0900

    [fix] change Entity id type in JpaRefreshTokenRepository & rootProject.name in setting.gradle

--- src/main/java/teamdevhub/devhub/DevhubApplication.java
+++ src/main/java/teamdevhub/devhub/DevhubApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@SpringBootApplication
 @EnableMethodSecurity
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareProvider")
-@SpringBootApplication
 public class DevhubApplication {
 
 	public static void main(String[] args) {
