0d4b264098dc63184a21bb66e837c868b5deca4f
Yu Sihwan <tlghks1217@naver.com>
2026-01-14 12:31:29 +0900

    [remove] unused import statement 제거

--- src/main/java/teamdevhub/devhub/domain/user/vo/UserSkill.java
+++ src/main/java/teamdevhub/devhub/domain/user/vo/UserSkill.java
@@ -1,6 +1,3 @@
 package teamdevhub.devhub.domain.user.vo;
 
-import teamdevhub.devhub.common.enums.ErrorCode;
-import teamdevhub.devhub.domain.exception.DomainRuleException;
-
 public record UserSkill(String userGuid, String skillCd) {}
