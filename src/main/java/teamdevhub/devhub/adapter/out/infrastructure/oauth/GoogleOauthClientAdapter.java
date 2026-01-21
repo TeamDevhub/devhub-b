4c0cf5b47639ea89a7454e1c0a2b9fb3ccfe815e
Yu Sihwan <tlghks1217@naver.com>
2026-01-21 14:24:09 +0900

    [feat] Oauth 기능 관련 어댑터 수정 및 Javadoc 작성

--- src/main/java/teamdevhub/devhub/adapter/out/infrastructure/oauth/GoogleOauthClientAdapter.java
+++ src/main/java/teamdevhub/devhub/adapter/out/infrastructure/oauth/GoogleOauthClientAdapter.java
@@ -26,11 +26,20 @@ public class GoogleOauthClientAdapter implements OauthClient {
     @Value("${oauth.google.redirect-uri}")
     private String redirectUri;
 
+    /**
+     * 해당 Oauth 로그인이 가능한지 응답해주는 OauthClient 공통 메서드
+     * @param verificationProvider 외부 인증 주체(ex 구글, 깃헙, 카카오)
+     * @return 사용가능 여부에 따라 true/false 반환
+     */
     @Override
     public boolean supports(VerificationProvider verificationProvider) {
         return verificationProvider == VerificationProvider.GOOGLE;
     }
 
+    /**
+     * 현재 작성된 소스 점검 필요
+     * @return 구글 로그인 페이지로 이동하는 리다이렉트 URL
+     */
     @Override
     public String getAuthorizationUrl() {
         return new GoogleAuthorizationCodeRequestUrl(clientId, redirectUri, List.of("profile", "email"))
@@ -39,9 +48,9 @@ public class GoogleOauthClientAdapter implements OauthClient {
     }
 
     /**
-     * Oauth 로 인증된 사용자를 가져오는 함수
-     * @param
-     * @return
+     * 인증된 사용자를 가져오는 함수, 추후 개발 필요
+     * @param code 인증 코드
+     * @return 외부 인증 주체가 제공하는 인증된 사용자
      */
     @Override
     public OauthUser fetchUser(String code) {
