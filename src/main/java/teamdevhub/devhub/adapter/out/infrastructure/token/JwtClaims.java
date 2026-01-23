af1c7266093445dd0cf0c58cc602382e39f61daa
Yu Sihwan <tlghks1217@naver.com>
2026-01-23 16:17:17 +0900

    [refactor] 불필요한 주석 제거

--- src/main/java/teamdevhub/devhub/adapter/out/infrastructure/token/JwtClaims.java
+++ src/main/java/teamdevhub/devhub/adapter/out/infrastructure/token/JwtClaims.java
@@ -9,6 +9,6 @@ public final class JwtClaims {
     public static final String USER_ROLE = "user_role";
     public static final String TOKEN_TYPE = "token_type";
 
-    public static final String SIGNUP_STATUS = "signup_status"; // optional
+    public static final String SIGNUP_STATUS = "signup_status";
     public static final String OAUTH_PROVIDER = "oauth_provider";
 }
