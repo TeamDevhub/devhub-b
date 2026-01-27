7b326b9a427eb9849824e2f1a19712e503f8a6ed
심세현 <ssehn9327@gmail.com>
2026-01-27 15:24:26 +0900

    [feat] 프로젝트(+스킬, 모집인원, 좋아요), 사용자평가, 알림, 배너, 첨부파일 엔티티 생성

---
+++ src/main/java/teamdevhub/devhub/adapter/out/attachmentFile/entity/AttachmentFileEntity.java
@@ -0,0 +1,48 @@
+package teamdevhub.devhub.adapter.out.attachmentFile.entity;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import teamdevhub.devhub.adapter.out.common.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@Table(
+        name = "attachment_file",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_file_guid",
+                        columnNames = "fileGuid"
+                )
+        }
+)
+public class AttachmentFileEntity extends BaseEntity {
+	
+	@Id
+	@Column(length = 32, nullable = false, unique = true)
+    private String fileGuid;
+
+    @Column(name = "original_name", nullable = false)
+    private String originalName;
+    
+    @Column(name = "extension_name", nullable = false)
+    private String extensionName;
+    
+    @Column(name = "size", nullable = false)
+    private Integer size;
+    
+    @Column(name = "path", nullable = false)
+    private String path;
+    
+}
---
+++ src/main/java/teamdevhub/devhub/adapter/out/project/entity/ProjectEntity.java
@@ -0,0 +1,83 @@
+package teamdevhub.devhub.adapter.out.project.entity;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import teamdevhub.devhub.adapter.out.common.converter.BooleanToYNConverter;
+import teamdevhub.devhub.adapter.out.common.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@Table(
+        name = "project",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_project_guid",
+                        columnNames = "projectGuid"
+                )
+        }
+)
+public class ProjectEntity extends BaseEntity{
+	
+	@Id
+	@Column(length = 32, nullable = false, unique = true)
+    private String projectGuid;
+
+    @Column(name = "file_guid", length = 32)
+    private String fileGuid;
+
+    @Column( name = "user_guid", length = 32, nullable = false)
+    private String userGuid;
+    
+    @Column(name = "recuritment_type_cd", length = 10, nullable = false)
+    private String recuritmentTypeCd;
+    
+    @Column(name = "progress_type_cd", length = 10, nullable = false)
+    private String progressTypeCd;
+    
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+    
+    @Column(name = "category", nullable = false)
+    private String category;
+    
+    @Column(name = "title", nullable = false)
+    private String title;
+    
+    @Column(name = "content", nullable = false)
+    private String content;
+    
+    @Column(name = "recuritment_start_date", nullable = false)
+    private LocalDateTime recuritmentStartDate;
+    
+    @Column(name = "recuritment_end_date", nullable = false)
+    private LocalDateTime recuritmentEndDate;
+    
+    @Column(name = "progress_start_date", nullable = false)
+    private LocalDateTime progressStartDate;
+    
+    @Column(name = "progress_end_date", nullable = false)
+    private LocalDateTime progressEndDate;
+    
+    @Column(nullable = false)
+    @Convert(converter = BooleanToYNConverter.class)
+    private boolean deleted;
+    
+    @Column( nullable = false)
+    @Convert(converter = BooleanToYNConverter.class)
+    private boolean capacityClosed;
+}
