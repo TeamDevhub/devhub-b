b5a14312c9ff3e6da3dabb9992effab2fb19606a
Goonerd17 <tlghks1217@naver.com>
2026-02-02 14:19:07 +0900

    [fix] 이메일 발송 관련 방어로직 추가

--- src/main/java/teamdevhub/devhub/core/auth/port/out/verification/VerificationRepository.java
+++ src/main/java/teamdevhub/devhub/core/auth/port/out/verification/VerificationRepository.java
@@ -3,8 +3,11 @@ package teamdevhub.devhub.core.auth.port.out.verification;
 import teamdevhub.devhub.core.auth.domain.Verification;
 import teamdevhub.devhub.core.auth.domain.vo.VerificationTarget;
 
+import java.time.LocalDateTime;
+
 public interface VerificationRepository {
 
+    boolean existsUnverifiedAndNotExpired(VerificationTarget verificationTarget, LocalDateTime now);
     void save(Verification verification);
     Verification findByVerificationTarget(VerificationTarget verificationTarget);
     void deleteByVerificationTarget(VerificationTarget verificationTarget);
--- src/main/java/teamdevhub/devhub/outbound/auth/persistence/JpaVerificationRepository.java
+++ src/main/java/teamdevhub/devhub/outbound/auth/persistence/JpaVerificationRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import teamdevhub.devhub.outbound.auth.adapter.entity.VerificationEntity;
 import teamdevhub.devhub.core.auth.domain.vo.VerificationType;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface JpaVerificationRepository extends JpaRepository<VerificationEntity, Long> {
 
-    Optional<VerificationEntity> findByVerificationTypeAndTargetValue(VerificationType verificationType, String targetValue);
+    boolean existsByVerificationTypeAndTargetValueAndExpiredAtAfterAndVerifiedFalse(VerificationType verificationType, String targetValue, LocalDateTime now);
+    Optional<VerificationEntity> findTopByVerificationTypeAndTargetValueOrderByExpiredAtDesc(VerificationType verificationType, String targetValue);
     void deleteByVerificationTypeAndTargetValue(VerificationType verificationType, String targetValue);
 }
