5df62f67357473a38e0d35f299029a8dc94704d2
정소영 <jungsoyy@5finger.co.kr>
2026-02-06 15:27:15 +0900

    공통코드 initialize 관련 삭제

--- src/main/java/teamdevhub/devhub/core/admin/code/port/out/CommonCodeRepository.java
+++ src/main/java/teamdevhub/devhub/core/admin/code/port/out/CommonCodeRepository.java
@@ -1,13 +1,4 @@
 package teamdevhub.devhub.core.admin.code.port.out;
 
-import java.util.List;
-
-import teamdevhub.devhub.outbound.admin.code.adapter.entity.CommonCodeEntity;
-
 public interface CommonCodeRepository {
-   boolean existsByCodeId(String codeId);
-   List<CommonCodeEntity> findBySuperiorCodeId(String superiorCodeId);
-   long count();
-   void save(CommonCodeEntity entity);
-   void saveAll(List<CommonCodeEntity> entities);
 }
