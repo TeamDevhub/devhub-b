25f0d934400792cb1999cbf51483927b8fa7ff1b
심세현 <ssehn9327@gmail.com>
2026-02-19 16:37:12 +0900

    [test] applicationFormService 테스트 코드 생성

---
+++ src/test/java/teamdevhub/devhub/small/core/project/application/ProjectServiceTest.java
@@ -0,0 +1,9 @@
+package teamdevhub.devhub.small.core.project.application;
+
+import teamdevhub.devhub.fake.pure.application.provider.FakeUuidIdentifierProvider;
+
+public class ProjectServiceTest {
+	
+	private FakeUuidIdentifierProvider identifierProvider;
+
+}
