927c825ab0d3cae1f68c3d9756429764d3fd9e82
Goonerd17 <tlghks1217@naver.com>
2026-03-08 22:39:37 +0900

    [fix] 프로젝트 컨트롤러 수정사항 원복

--- src/main/java/teamdevhub/devhub/api/project/controller/ProjectController.java
+++ src/main/java/teamdevhub/devhub/api/project/controller/ProjectController.java
@@ -25,7 +25,7 @@ public class ProjectController {
 	
 	@PostMapping
 	public ResponseEntity<DataApiResponseDto<Void>> createProject(@Valid @RequestBody CreateProjectRequestDto createProjectRequestDto, @LoginUser AuthenticatedUser authenticatedUser) {
-		projectFacade.createProject(createProjectRequestDto.toCommand(authenticatedUser.userGuid(), ""));
+		projectFacade.createProject(createProjectRequestDto.toCommand(authenticatedUser.userGuid(), authenticatedUser.username()));
         return ResponseEntity.ok(
                 DataApiResponseDto.successWithoutData(
                         SuccessCode.CREATE_SUCCESS
